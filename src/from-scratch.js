57cb4fc3be9e74cfd81f0d1373c3e3d033d1d24c
kellyxc28 <kellyxiongc@gmail.com>
2024-04-03 15:33:51 -0400

    Completed Question 1: measureRain()

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,4 +1,19 @@
-const measureRain = () => {
+const measureRain = (inches) => {
+  if (inches === 0) {
+    console.log('drought');
+  }
+  else if (inches < 2) {
+    console.log('dry');
+  }
+  else if (inches < 4) {
+    console.log('average');
+  }
+  else if (inches < 6) {
+    console.log('rainy');
+  }
+  else if (inches >= 6) {
+    console.log('flood');
+  }
 };
 
 const happyBirthdayPet = () => {
