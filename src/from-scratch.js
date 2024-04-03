98dd0647dd38042aacac52811df82fb5f62bead0
kellyxc28 <kellyxiongc@gmail.com>
2024-04-03 18:02:41 -0400

    Completed Question 3: funTypes()

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -40,8 +40,42 @@ const happyBirthdayPet = (breed, age) => {
     }
 };
 
-const funTypes = () => {
+const funTypes = (jsType) => {
+    if (typeof jsType === 'string') {
+        console.log('That\'s just some text.');
+    }
+    else if (Number.isNaN(jsType)) {              // special case: under number 
+      console.log('Well, now you\'re just showing off.');
+    }
+    else if (typeof jsType === 'number') {
+      console.log('That\'s a good number.');
+    }
+    else if (typeof jsType === 'boolean') {
+      console.log('To bool, or not to bool?');
+    }
+    else if (typeof jsType === 'undefined') {
+      console.log('Nothing, but I didn\'t set that.');
+    }
+    else if (jsType === null) {                  // special case: no ' ' needed, under object
+      console.log('Nothing, and I did set that.');
+    }
+    else if (Array.isArray(jsType)) {           // special case: under object 
+      console.log('I order you to be indexed.');
+    }
+    else if (typeof jsType === 'object') {      // special case: under object   
+      console.log('Anybody got the key?');
+    }
 };
+
+// For Testing 
+// funTypes("Hello");    // Output: That's just some text.
+// funTypes(42);         // Output: That's a good number.
+// funTypes(true);       // Output: To bool, or not to bool?
+// funTypes(undefined);  // Output: Nothing, but I didn't set that.
+// funTypes(null);       // Output: Nothing, and I did set that.
+// funTypes({});         // Output: Anybody got the key?
+// funTypes([1, 2, 3]);  // Output: I order you to be indexed.
+// funTypes(NaN);        // Output: Well, now you're just showing off.
 
 const rounder = () => {
 };
