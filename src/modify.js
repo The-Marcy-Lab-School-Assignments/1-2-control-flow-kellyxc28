548f862397f8ef117fa6aa8528459886058efb9b
kellyxc28 <kellyxiongc@gmail.com>
2024-04-03 18:36:20 -0400

    Completed Question 6: MODIFY - wildlyBiasedReview()

--- src/modify.js
+++ src/modify.js
@@ -1,10 +1,10 @@
 const wildlyBiasedReview = (location) => {
-  if (location === 'NYC') {
+  if (location !== 'NYC') {
+    return console.log('Yea that place is cool I guess');
+  } else {
     console.log('THE GREATEST CITY IN THE WORLD');
     console.log('THE CITY THAT NEVER SLEEPS');
     console.log('WOW WHAT A CITY');
-  } else {
-    console.log('Yea that place is cool I guess');
   }
 };
 
