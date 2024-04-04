7358b37b98729352599efca7a7beabbb5130b0b5
kellyxc28 <kellyxiongc@gmail.com>
2024-04-04 18:18:24 -0400

    Completed Bonus: measureRainSwitch()

--- src/bonus-switch.js
+++ src/bonus-switch.js
@@ -1,4 +1,39 @@
-const measureRainSwitch = () => {
+const measureRainSwitch = (inches) => {
+    switch (inches) {
+      case 0:
+        console.log('drought');
+        break;
+      case 1:
+        console.log('dry');
+        break;
+      case 2:
+        console.log('average');
+        break;
+      case 3:
+        console.log('average');
+        break;  
+      case 4:
+        console.log('rainy');
+        break;
+      case 5:
+        console.log('rainy');
+        break;
+      case 6:
+        console.log('flood');
+        break;  
+      case 7:
+        console.log('flood');
+        break; 
+      case 8:
+        console.log('flood');
+        break; 
+      case 9:
+        console.log('flood');
+        break;   
+      case 10:
+        console.log('flood');
+        break;
+    }
 };
 
 const rounderSwitch = () => {
