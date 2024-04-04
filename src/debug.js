c967178bbbee93baf30c14745ce85e7d08040211
kellyxc28 <kellyxiongc@gmail.com>
2024-04-04 13:55:49 -0400

    Completed Question 9: DEBUG - funkoPopAddictionLevel()

--- src/debug.js
+++ src/debug.js
@@ -11,13 +11,13 @@ const coolnessGauge = (numOfFridges) => {
 const funkoPopAddictionLevel = (numOfFunkoPops) => {
   if (numOfFunkoPops === 0) {
     console.log('No pops? Maybe try one.');
-  } else if (numOfFunkoPops >= 1) {
+  } else if (numOfFunkoPops >= 1 && numOfFunkoPops <= 10) {
     console.log('Only a few? Keep having fun!');
-  } else if (numOfFunkoPops > 10) {
+  } else if (numOfFunkoPops > 10 && numOfFunkoPops <= 20) {
     console.log('You have a problem.');
-  } else if (numOfFunkoPops > 20) {
+  } else if (numOfFunkoPops > 20 && numOfFunkoPops <= 30) {
     console.log('You need help!');
-  } else {
+  } else {    // numOfFunkoPops > 30 
     console.log('You need an intervention!!!');
   }
 };
